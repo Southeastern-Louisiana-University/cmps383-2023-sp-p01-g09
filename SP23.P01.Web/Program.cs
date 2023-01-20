bd610d946d044bf5d24e6d14bc1fa6c75486d7bd
scethen <Kyle.Tran@selu.edu>
2023-01-20 13:12:02 -0600

    trying to update and start entity setup

--- SP23.P01.Web/Program.cs
+++ SP23.P01.Web/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
