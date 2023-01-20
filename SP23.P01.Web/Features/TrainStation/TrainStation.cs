8e266f223aa371eed0449c77c805bd45a1cf9abc
NRanel <99266490+NRanel@users.noreply.github.com>
2023-01-19 23:33:45 -0600

    train station dto

---
+++ SP23.P01.Web/Features/TrainStation/TrainStation.cs
@@ -0,0 +1,13 @@
+﻿namespace SP23.P01.Web.Features.TrainStation
+{
+    public class TrainStation
+    {
+    }
+    public class TrainStationDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+    }
+}
