e95c3236b8182860a5683214aca1483f2e73d7e1
NRanel <99266490+NRanel@users.noreply.github.com>
2023-01-23 23:45:31 -0600

    Get Endpoint

---
+++ SP23.P01.Web/Controllers/TrainStationController.cs
@@ -0,0 +1,37 @@
+﻿
+using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using SP23.P01.Web.Features.TrainStation;
+
+namespace SP23.P01.Web.Controllers
+{
+    [ApiController]
+    [Route("api/stations")]
+    public class TrainStationController : Controller
+    {
+        private DataContext _dataContext;
+        public TrainStationController(DataContext context)
+        {
+            _dataContext = context;
+        }
+
+        [HttpGet]
+        public ActionResult GetStations()
+        {
+            var returnedStations = _dataContext.TrainStations.Select(x => new TrainStationDto
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Address = x.Address
+            }).ToList();
+
+            return Ok(returnedStations);
+        }
+
+       /* public IActionResult Index()
+        {
+            return View();
+        }*/
+    }
+}
