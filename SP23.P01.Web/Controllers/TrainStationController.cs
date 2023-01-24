8d88627ab9fca99fbd34f5b2c0738284d3c19a97
NRanel <99266490+NRanel@users.noreply.github.com>
2023-01-24 17:19:24 -0600

    Get by id endpoint

--- SP23.P01.Web/Controllers/TrainStationController.cs
+++ SP23.P01.Web/Controllers/TrainStationController.cs
@@ -29,6 +29,32 @@ namespace SP23.P01.Web.Controllers
             return Ok(returnedStations);
         }
 
+        [HttpGet("{id}")]
+        public ActionResult GetStationById(int id)
+        {
+            List<String> Errors = new List<String>();
+            if (id <= 0)
+            {
+                Errors.Add("Ids are only integers 1 and up.");
+                return BadRequest(Errors);
+            }
+            var matchingStation = _dataContext.TrainStations.FirstOrDefault(x => x.Id == id); 
+            if (matchingStation == null)
+            {
+                Errors.Add("The train station the user was seeking does not exist.");
+                return NotFound(Errors);
+            }
+            else
+            {
+                var returnStation = new TrainStationDto
+                {
+                    Id = matchingStation.Id,
+                    Name = matchingStation.Name,
+                    Address = matchingStation.Address
+                };
+                return Ok(returnStation);
+            }
+        }
        /* public IActionResult Index()
         {
             return View();
